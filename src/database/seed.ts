9481a319890759a04d1f24e7e574327ab9439c93
hadi arbabi <hadimojak@gmail.com>
2026-07-01 17:38:10 +0330

    jwt

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -3,33 +3,56 @@ import { AppDataSource } from './data-source';
 import { TicketEntity } from './../ticket/ticket.entity';
 import * as fs from 'fs';
 import * as path from 'path';
+import { chunk } from 'lodash';
 
 async function seed() {
   try {
-    const filePath = path.resolve(process.cwd(), 'samplData.json');
+    const filePathArr: string[] = [];
+    const filePath1 = path.resolve(process.cwd(), 'day-ticket-1.json');
+    filePathArr.push(filePath1);
+    const filePath2 = path.resolve(process.cwd(), 'day-ticket-2.json');
+    filePathArr.push(filePath2);
+    const filePath3 = path.resolve(process.cwd(), 'day-ticket-3.json');
+    filePathArr.push(filePath3);
 
-    if (!fs.existsSync(filePath)) {
-      throw new Error(`Seed file not found: ${filePath}`);
+    for (let index = 0; index < filePathArr.length; index++) {
+      const filePath = filePathArr[index];
+      if (!fs.existsSync(filePath)) {
+        throw new Error(`Seed file not found: ${filePath}`);
+      }
+
+      const raw = fs.readFileSync(filePath, 'utf8');
+      const { tickets } = JSON.parse(raw);
+
+      if (!Array.isArray(tickets)) {
+        throw new Error('samplData.json must contain an array of tickets');
+      }
+
+      await AppDataSource.initialize();
+
+      const repo = AppDataSource.getRepository(TicketEntity);
+
+      console.log(`Seeding ${tickets.length} tickets...`);
+
+      const chunkSize = 100;
+      const chunks = chunk(tickets, chunkSize);
+
+      for (const batch of chunks) {
+        // console.log({ batch });
+        const sanitizedBatch = batch.map((ticket) => ({
+          ...ticket,
+          closeddate: ticket.closeddate === '' ? null : ticket.closeddate,
+          createdtime: ticket.createdtime === '' ? null : ticket.createdtime,
+          accountname: ticket.accountname === '' ? null : ticket.accountname,
+        }));
+
+        await repo.insert(sanitizedBatch);
+      }
+
+      console.log('✅ Seed completed successfully');
+
+      await AppDataSource.destroy();
     }
-
-    const raw = fs.readFileSync(filePath, 'utf8');
-    const tickets = JSON.parse(raw);
-
-    if (!Array.isArray(tickets)) {
-      throw new Error('samplData.json must contain an array of tickets');
-    }
-
-    await AppDataSource.initialize();
-
-    const repo = AppDataSource.getRepository(TicketEntity);
-
-    console.log(`Seeding ${tickets.length} tickets...`);
-
-    await repo.insert(tickets);
-
-    console.log('✅ Seed completed successfully');
-
-    await AppDataSource.destroy();
     process.exit(0);
   } catch (error) {
     console.error('❌ Seed failed');
